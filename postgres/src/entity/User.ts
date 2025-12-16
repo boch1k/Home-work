676fada877dcbd52286988e585e5e455971c3895
boch1k <147732642+boch1k@users.noreply.github.com>
2025-12-16 05:21:04 +0300

    Create User.ts

---
+++ postgres/src/entity/User.ts
@@ -0,0 +1,20 @@
+import { Entity, PrimaryGeneratedColumn, Column, JoinColumn, OneToMany, Relation } from "typeorm";
+import { Photo } from "./Photo";
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number
+
+  @Column()
+  firstName: string
+
+  @Column()
+  lastName: string
+
+  @Column()
+  age: number
+
+  @OneToMany(() => Photo, (photo) => photo.user)
+  photos: Relation<Photo[]>
+}
